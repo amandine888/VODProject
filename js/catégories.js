6df27d32fe4d731adabd61c4369e2379fc844775
Your Name <you@example.com>
2019-11-05 21:28:46 +0100

    page categorie presque fini

---
+++ js/catégories.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+    
+
+$(".listecatégories li").mouseenter(function(){
+  $(this).css({"text-decoration-line":"line-through"});
+  })
+$(".listecatégories li").mouseleave(function(){
+  $(this).css("text-decoration-line","none");
+});
+
+
+$("nav li").mouseenter(function(){
+  $(this).css({"border":"1px solid white"});
+  })
+$("nav li").mouseleave(function(){
+  $(this).css("border","none");
+});
+
+
+$("#misora").mouseenter(function(){
+  $(this).css("border","none");
+});
+
+
+
+});
