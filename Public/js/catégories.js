5734c4ba5656c4cfd998a81ee7dcb212333de1e6
Your Name <you@example.com>
2019-11-22 16:44:49 +0100

    debut requete

--- Public/js/catégories.js
+++ Public/js/catégories.js
@@ -1,6 +1,5 @@
 $( document ).ready(function() {
     
-
 $(".listecatégories li").mouseenter(function(){
   $(this).css({"text-decoration-line":"line-through"});
   })
@@ -9,12 +8,12 @@ $(".listecatégories li").mouseleave(function(){
 });
 
 
-$("nav li").mouseenter(function(){
-  $(this).css({"border":"1px solid white"});
-  })
-$("nav li").mouseleave(function(){
-  $(this).css("border","none");
-});
+// $("nav li").mouseenter(function(){
+//   $(this).css({"border":"1px solid white"});
+//   })
+// $("nav li").mouseleave(function(){
+//   $(this).css("border","none");
+// });
 
 
 $("#misora").mouseenter(function(){
@@ -29,9 +28,73 @@ $(".icone").click(function(){
 	else{
 		$("#listeuser").css("display","none")
 	}
+})
 
+
+if($(".middlefilms").css("display")==="flex"){
+  $("#categoriesfilms").addClass("selected")}
+
+
+$("#categoriesfilms").click(function(){
+   $("#cat").text("Films");
+   $("#categoriesfilms").addClass("selected")
+   $("#categoriesseries, #categoriesanimes, #categoriesrealisateurs").removeClass("selected")
+  if($(".middlefilms").css("display") === "none"){
+    $(".middleseries .middleanimes .middlerealisateurs").css("display")==="none";
+    $(".middlefilms").css("display") === "flex";
+  }
+})
+
+  
+
+$("#categoriesseries").click(function(){
+  $("#cat").text("Séries");
+  $("#categoriesseries").addClass("selected")
+   $("#categoriesfilms, #categoriesanimes, #categoriesrealisateurs").removeClass("selected")
+  if($(".middleseries").css("display")==="none"){
+    $(".middleseries").css("display","flex");
+    $(".middlefilms, .middleanimes, .middlerealisateurs").css("display","none");
+  }
 
 })
+
+
+
+$("#categoriesanimes").click(function(){
+   $("#cat").text("Animés");
+   $("#categoriesanimes").addClass("selected")
+   $("#categoriesseries, #categoriesfilms, #categoriesrealisateurs").removeClass("selected")
+  if($(".middleanimes").css("display")==="none"){
+    $(".middleanimes").css("display","flex");
+    $(".middlefilms, .middleseries, .middlerealisateurs").css("display","none");
+  }
+})
+
+
+
+$("#categoriesrealisateurs").click(function(){
+   $("#cat").text("Réalisateurs");
+   $("#categoriesrealisateurs").addClass("selected")
+   $("#categoriesseries, #categoriesanimes, #categoriesfilms").removeClass("selected")
+  if($(".middlerealisateurs").css("display")==="none"){
+    $(".middlerealisateurs").css("display","flex");
+    $(".middlefilms, .middleanimes, .middleseries").css("display","none");
+  }
+})
+
+
+$("#categoriesfilms").click(function(){
+  var url = $.get("https:brianboudrioux.fr/simplon/api/products");
+  $.get("https://brianboudrioux.fr/simplon/api/products", function(test){
+    for (var i = 0; i < url.length; i++) {
+      return url[i];
+    }
+    console.log(url[i])
+  });
+});
+  
+
+
 
 
 
