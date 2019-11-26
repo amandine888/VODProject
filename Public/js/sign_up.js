d1a1d0bbee9b9826efeb8a6d8aeee7b016318967
Amandine <amandine2.divay@gmail.com>
2019-11-26 16:34:08 +0100

    second step get values page sign-up

--- Public/js/sign_up.js
+++ Public/js/sign_up.js
@@ -16,6 +16,8 @@ window.onload = function() {
 
     const submitBasic = document.getElementById("BoutonBasic"); 
     const basicForm = document.getElementById("BasicFormulaire"); 
+    const submitPremium = document.getElementById("BoutonPremium"); 
+    const premiumForm = document.getElementById("PremiumFormulaire"); 
 
     submitBasic.addEventListener("click", function(){
             let username = basicForm[3].value; 
@@ -29,4 +31,19 @@ window.onload = function() {
 
             let verifPassword = basicForm[6].value; 
             console.log(verifPassword);
-})}
+        })
+
+        
+    submitPremium.addEventListener("click", function(){
+            let username2 = premiumForm[3].value; 
+            console.log(username2); 
+                    
+            let email2 = premiumForm[4].value; 
+            console.log(email2); 
+        
+            let password2 = premiumForm[5].value; 
+            console.log(password2);
+        
+            let verifPassword2 = premiumForm[6].value; 
+            console.log(verifPassword2); 
+        })}
