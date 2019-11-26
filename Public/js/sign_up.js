20556d8c95441929f9698b3365bdcab1290c6064
Amandine <amandine2.divay@gmail.com>
2019-11-26 14:38:48 +0100

    get values from sign.up form

--- Public/js/sign_up.js
+++ Public/js/sign_up.js
@@ -1,6 +1,6 @@
-$(document).ready(function(){
+window.onload = function() {
 
-    $("#AbonnementsContainer, #Switch, h3").on("click", function(){
+    $("#AbonnementsContainer #Switch h3").on("click", function(){
 
         if ($("#Premium").css ("display")==="none"){
             $("#Premium").css("display", "flex"); 
@@ -13,4 +13,20 @@ $(document).ready(function(){
             $("#Switch h3").text("Switch pour l'abonnement Premium - 11€/mois")
         }
     })
-})
+
+    const submitBasic = document.getElementById("BoutonBasic"); 
+    const basicForm = document.getElementById("BasicFormulaire"); 
+
+    submitBasic.addEventListener("click", function(){
+            let username = basicForm[3].value; 
+            console.log(username); 
+            
+            let email = basicForm[4].value; 
+            console.log(email); 
+
+            let password = basicForm[5].value; 
+            console.log(password);
+
+            let verifPassword = basicForm[6].value; 
+            console.log(verifPassword);
+})}
